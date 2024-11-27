da30653f3b44eed27b533e647d606675449bb680
asif <mdasifraza820@gmail.com>
2024-11-27 09:14:14 +0530

    updated

--- controllers/stTablesController.js
+++ controllers/stTablesController.js
@@ -36,4 +36,130 @@ const getBuildersListBasedOnCityId=async(req,res)=>{
     }
 }
 
-module.exports={getAllStateList,getCityListBasedOnStateId,getBuildersListBasedOnCityId};
+const getStaticTablesData=async(req,res)=>{
+    try{
+        const [depositeRangeList]=await db.query(`call GetDepositRanges()`)
+        const [conversationModesList]=await db.query(`call GetConversationModes`)
+        const [bedroomsList]=await db.query(`call GetBedrooms`)
+        const [bathroomsList]=await db.query(`call GetBathrooms`)
+        const [balconiesList]=await db.query(`call GetBalconies`)
+        const [floorRangesList]=await db.query(`call GetFloorRanges`)
+        const [homeTypesList]=await db.query(`call GetHomeTypes`)
+        const [propertyDescriptionList]=await db.query(`call GetPropertyDescriptions`)
+        const [propertyTypesList]=await db.query(`call GetPropertyTypes`)
+        const [rentalRangesList]=await db.query(`call GetRentalRanges`)
+        const [TenantEatPreferencesList]=await db.query(`call GetTenantEatPreferences`)  
+        const [GetTenantTypesList]=await db.query(`call GetTenantTypes`)
+
+        const result={
+            "depositeRangeList":depositeRangeList[0],
+            "conversationModesList":conversationModesList[0],
+            "bedroomsList":bedroomsList[0],
+            "bathroomsList":bathroomsList[0],
+            "balconiesList":balconiesList[0],
+            "floorRangesList":floorRangesList[0],
+            "homeTypesList":homeTypesList[0],
+            "propertyDescriptionList":propertyDescriptionList[0],
+            "propertyTypesList":propertyTypesList[0],
+            "rentalRangesList":rentalRangesList[0],
+            "TenantEatPreferencesList":TenantEatPreferencesList[0],
+            "GetTenantTypesList":GetTenantTypesList[0]
+
+        }
+        console.log("4",result);
+        return res.status(200).json(result)
+    }
+    catch(error){
+        return res.status(500).json({ message: 'Error getting builders list', error: error.message });
+    }
+}
+
+/*const getBuildersListBasedOnCityId=async(req,res)=>{
+    const {city_id}=req.query
+    try{
+        const [builders_list]=await db.query(`call get_bulders_list_based_on_cityId(?)`,[city_id])
+        console.log(builders_list)
+        return res.status(200).json(builders_list[0])
+    }
+    catch(error){
+        return res.status(500).json({ message: 'Error getting builders list', error: error.message });
+    }
+}
+const getBuildersListBasedOnCityId=async(req,res)=>{
+    const {city_id}=req.query
+    try{
+        const [builders_list]=await db.query(`call get_bulders_list_based_on_cityId(?)`,[city_id])
+        console.log(builders_list)
+        return res.status(200).json(builders_list[0])
+    }
+    catch(error){
+        return res.status(500).json({ message: 'Error getting builders list', error: error.message });
+    }
+}
+const getBuildersListBasedOnCityId=async(req,res)=>{
+    const {city_id}=req.query
+    try{
+        const [builders_list]=await db.query(`call get_bulders_list_based_on_cityId(?)`,[city_id])
+        console.log(builders_list)
+        return res.status(200).json(builders_list[0])
+    }
+    catch(error){
+        return res.status(500).json({ message: 'Error getting builders list', error: error.message });
+    }
+}
+const getBuildersListBasedOnCityId=async(req,res)=>{
+    const {city_id}=req.query
+    try{
+        const [builders_list]=await db.query(`call get_bulders_list_based_on_cityId(?)`,[city_id])
+        console.log(builders_list)
+        return res.status(200).json(builders_list[0])
+    }
+    catch(error){
+        return res.status(500).json({ message: 'Error getting builders list', error: error.message });
+    }
+}
+const getBuildersListBasedOnCityId=async(req,res)=>{
+    const {city_id}=req.query
+    try{
+        const [builders_list]=await db.query(`call get_bulders_list_based_on_cityId(?)`,[city_id])
+        console.log(builders_list)
+        return res.status(200).json(builders_list[0])
+    }
+    catch(error){
+        return res.status(500).json({ message: 'Error getting builders list', error: error.message });
+    }
+}
+const getBuildersListBasedOnCityId=async(req,res)=>{
+    const {city_id}=req.query
+    try{
+        const [builders_list]=await db.query(`call get_bulders_list_based_on_cityId(?)`,[city_id])
+        console.log(builders_list)
+        return res.status(200).json(builders_list[0])
+    }
+    catch(error){
+        return res.status(500).json({ message: 'Error getting builders list', error: error.message });
+    }
+}
+const getBuildersListBasedOnCityId=async(req,res)=>{
+    const {city_id}=req.query
+    try{
+        const [builders_list]=await db.query(`call get_bulders_list_based_on_cityId(?)`,[city_id])
+        console.log(builders_list)
+        return res.status(200).json(builders_list[0])
+    }
+    catch(error){
+        return res.status(500).json({ message: 'Error getting builders list', error: error.message });
+    }
+}
+
+const callStoredProcedure = (procedureName) => (req, res) => {
+    db.query(`CALL ${procedureName}()`, (error, results) => {
+      if (error) {
+        console.error(`Error executing procedure ${procedureName}:`, error);
+        return res.status(500).json({ error: 'Internal Server Error' });
+      }
+      res.json(results[0]);
+    });
+  };*/
+
+module.exports={getAllStateList,getCityListBasedOnStateId,getBuildersListBasedOnCityId,getStaticTablesData};
--- routes/stTablesRoutes.js
+++ routes/stTablesRoutes.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const router = express.Router();
-const { getAllStateList, getCityListBasedOnStateId,getBuildersListBasedOnCityId} = require('../controllers/stTablesController');
+const { getAllStateList, getCityListBasedOnStateId,getBuildersListBasedOnCityId,getStaticTablesData} = require('../controllers/stTablesController');
 
 
 
-router.get('/stateList', getAllStateList); // Fetch properties
+router.get('/stateList', getAllStateList);
 router.get('/cityList', getCityListBasedOnStateId);
 router.get('/buildersList', getBuildersListBasedOnCityId);
+router.get('/stTablesData', getStaticTablesData);
+
+
 
 module.exports = router;
